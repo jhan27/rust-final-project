f78a2b4c6d90e4cfb6d46d5cb57796d3f49b8ac0
Jin Han <jinhan2019@u.northwestern.edu>
2019-06-03 23:58:06 -0500

    flip implemented and some code clean up

--- src/image.rs
+++ src/image.rs
@@ -1,13 +1,14 @@
 use image as rust_image;
 use rayon::prelude::*;
-use image::{DynamicImage, ImageBuffer, GenericImageView};
-use core::borrow::Borrow;
-use std::{borrow::Cow::Owned,
-          cmp};
+use image::{DynamicImage, GenericImageView};
+use std::{cmp};
 use snafu::{Snafu, ResultExt};
-use std::sync::mpsc::TryRecvError::Disconnected;
 
 type Matrix<T> = Box<[T]>;
+
+
+/******************************* OwnedImage Struct and Member Functions *******************************/
+/******************************************************************************************************/
 
 /// An owned image.
 pub struct OwnedImage {
@@ -27,23 +28,67 @@ pub struct Pixel {
     alpha: u8
 }
 
-/// A borrowed slice of an image.
-pub struct ImageSlice<'a> {
-    pixels: &'a [u8],
-    spec: SliceSpec,
+impl OwnedImage {
+
+    pub fn new (dims: Dimensions, pixels: Matrix<u8>) -> OwnedImage {
+        OwnedImage {
+            dims,
+            pixels,
+        }
+    }
+
+    pub fn import(image:DynamicImage) -> OwnedImage {
+        let (w, h) = image.dimensions();
+        OwnedImage {
+            dims: Dimensions { width: w, height: h},
+            pixels: image.raw_pixels().into_boxed_slice(),
+        }
+    }
+
+    pub fn get_pixels(&self) -> &[u8] {
+        &self.pixels
+    }
+
+    pub fn get_pixel(&self, x: u32, y: u32) -> Pixel {
+        let w = self.dims.width;
+        let h = self.dims.height;
+
+        if x >= w && y >= h{
+            dispatch_error(ImageError::IndexOutOfBound)
+        }
+        let index = ((y*w*4) + 4 * x) as usize;
+        Pixel {
+            r: self.pixels[index],
+            g: self.pixels[index+1],
+            b: self.pixels[index+2],
+            alpha: self.pixels[index+3],
+        }
+    }
+
+    pub fn set_pixel(&mut self, x: u32, y: u32, pix: Pixel) {
+        let w = self.dims.width;
+        let h = self.dims.height;
+        if x >= w || y >= h{
+            dispatch_error(ImageError::IndexOutOfBound)
+        }
+        let index = ((y*w*4) + 4 * x) as usize;
+        self.pixels[index] = pix.r;
+        self.pixels[index+1] = pix.g;
+        self.pixels[index+2] = pix.b;
+        self.pixels[index+3] = pix.alpha;
+    }
+
+    pub fn save_image(&self){
+        let image = match image::save_buffer("processed.png", &self.pixels,
+                                             self.dims.width, self.dims.height, image::RGBA(8)) {
+            Ok(img) => {img}
+            Err(err) => return dispatch_error(ImageError::ImageOperationFailed)
+        };
+    }
 }
 
-/// A mutable, borrowed slice of an image.
-pub struct ImageSliceMut<'a> {
-    pixels: &'a mut [u8],
-    spec: SliceSpec,
-}
-
-pub struct SliceSpec {
-    offset: (u32, u32),
-    stride: (u32, u32),
-    dims: Dimensions,
-}
+/******************************************* Image Trait **********************************************/
+/******************************************************************************************************/
 
 /// Immutable image operations.
 pub trait Image {
@@ -63,79 +108,6 @@ pub trait Image {
     fn flipped(&self, horiz: bool, vert: bool) -> Self::Owned;
 }
 
-impl OwnedImage {
-    pub fn new(image:DynamicImage) -> OwnedImage {
-        let (w, h) = image.dimensions();
-        OwnedImage {
-            dims: Dimensions { width: w, height: h},
-            pixels: image.raw_pixels().into_boxed_slice(),
-        }
-    }
-
-    pub fn get_pixels(&self) -> &[u8] {
-        &self.pixels
-    }
-
-    pub fn get_pixel(&self, x: u32, y: u32) -> Pixel{
-        let w = self.dims.width;
-        let h = self.dims.height;
-
-        if x < w && y < h{
-            dispatch_error(ImageError::IndexOutOfBound)
-        }
-        let index = ((x*w*4) + 4 * y) as usize;
-        Pixel {
-            r: self.pixels[index],
-            g: self.pixels[index+1],
-            b: self.pixels[index+2],
-            alpha: self.pixels[index+3],
-        }
-    }
-
-    pub fn set_pixel(&mut self, x: u32, y: u32, pix: Pixel) {
-        let w = self.dims.width;
-        let h = self.dims.height;
-        if x < w && y < h{
-            dispatch_error(ImageError::IndexOutOfBound)
-        }
-        let index = ((x*w*4) + 4 * y) as usize;
-        self.pixels[index] = pix.r;
-        self.pixels[index+1] = pix.g;
-        self.pixels[index+2] = pix.b;
-        self.pixels[index+3] = pix.alpha;
-    }
-}
-
-impl<'a> ImageSlice<'a> {
-
-    pub fn generate_image(&self) {
-        let (off_x, off_y)= self.spec.offset;
-        let (w, h) = self.spec.stride;
-        let dims = &self.spec.dims;
-
-        let mut buffer = Vec::new();
-
-        for j in 0..dims.height {
-            let offset = w * j * 4 + off_x + off_y*w*4;
-            for i in 0..dims.width * 4 {
-                buffer.push(self.pixels[(offset+i) as usize]);
-            }
-        }
-
-        let image = match image::save_buffer("image.png", &buffer.into_boxed_slice(),
-                                             dims.width, dims.height, image::RGBA(8)) {
-            Ok(img) => {img}
-            Err(err) => return dispatch_error(ImageError::CropFailed)
-        };
-
-        let img = match rust_image::open("image.png") {
-            Ok(img) => img,
-            Err(err) => return dispatch_error(ImageError::InvalidFormat)
-        };
-
-//        println!("after crop image is: {:?}", img.raw_pixels());
-    }
-}
 
 impl Image for OwnedImage{
 
@@ -153,11 +125,12 @@ impl Image for OwnedImage{
         let w = cmp::min(dims.width, img_dims.width - x);
         let h = cmp::min(dims.height, img_dims.height - y);
 
-        let slice_spec = SliceSpec {offset: (x*4, y*4),
-            dims: dims,
-            stride: (img_dims.width, img_dims.height)};
+        let spec = SliceSpec {offset: (x*4, y*4),
+            dims: Dimensions{ width: w, height: h},
+            stride: (img_dims.width, img_dims.height),
+            orientation: Orientation:: I};
 
-        ImageSlice {pixels: self.get_pixels(), spec: slice_spec}
+        ImageSlice {pixels: &self.get_pixels(), spec}
     }
 
     fn blurred(&self, amount: u32) -> Self::Owned {
@@ -165,19 +138,37 @@ impl Image for OwnedImage{
     }
 
     fn flipped(&self, horiz: bool, vert: bool) -> Self::Owned {
-        unimplemented!()
+        let width = self.dims.width;
+        let height = self.dims.height;
+
+        let mut pixels = Vec::new();
+
+        for j in 0..height {
+            for i in 0..width {
+                let p;
+                if horiz && vert {
+                    p = self.get_pixel(width-i-1, height-j-1);
+                } else if horiz {
+                    p = self.get_pixel(width-i-1, j);
+                } else if vert {
+                    p = self.get_pixel(i, height-j-1);
+                } else {
+                    p = self.get_pixel(i, j);
+                }
+                pixels.push(p.r);
+                pixels.push(p.g);
+                pixels.push(p.b);
+                pixels.push(p.alpha);
+            }
+        }
+
+        OwnedImage:: new(Dimensions{ width, height}, pixels.into_boxed_slice())
     }
 }
 
 
-/// Four quadrants of an image.
-pub struct SplitMut<'a> {
-    pub top_left: ImageSliceMut<'a>,
-    pub bottom_left: ImageSliceMut<'a>,
-    pub top_right: ImageSliceMut<'a>,
-    pub bottom_right: ImageSliceMut<'a>,
-
-}
+/***************************************** ImageMut Trait *********************************************/
+/******************************************************************************************************/
 
 pub trait ImageMut: Image {
     /// Mutable borrows a sub-image.
@@ -212,11 +203,12 @@ pub trait ImageMut: Image {
     /// Copies into this image from another, existing image.
     /// (Do dimensions need to match?)
     fn blit_from(&mut self, source: impl Image);
-
 }
 
 impl ImageMut for OwnedImage {
-    fn crop_mut(&mut self, x: u32, y: u32, dims: Dimensions) -> ImageSliceMut {unimplemented!()}
+    fn crop_mut(&mut self, x: u32, y: u32, dims: Dimensions) -> ImageSliceMut {
+        unimplemented!()
+    }
 
     fn split_mut(&mut self, x: u32, y: u32) -> SplitMut {unimplemented!()}
 
@@ -230,7 +222,6 @@ impl ImageMut for OwnedImage {
     }
 
     fn flip_from(&mut self, horiz: bool, vert: bool, source: impl Image) {
-
         self.blit_from(source);
         self.flip(horiz, vert);
     }
@@ -238,9 +229,63 @@ impl ImageMut for OwnedImage {
     fn blit_from(&mut self, source: impl Image) {unimplemented!()}
 }
 
+/***************************************** Other Structs **********************************************/
+/******************************************************************************************************/
+
+/// A borrowed slice of an image.
+pub struct ImageSlice<'a> {
+    pixels: &'a [u8],
+    spec: SliceSpec,
+}
+
+/// A mutable, borrowed slice of an image.
+pub struct ImageSliceMut<'a> {
+    pixels: &'a mut [u8],
+    spec: SliceSpec,
+}
+
+pub struct SliceSpec {
+    offset: (u32, u32),
+    stride: (u32, u32),
+    dims: Dimensions,
+    orientation: Orientation,
+}
+
+/// Four quadrants of an image.
+pub struct SplitMut<'a> {
+    pub top_left: ImageSliceMut<'a>,
+    pub bottom_left: ImageSliceMut<'a>,
+    pub top_right: ImageSliceMut<'a>,
+    pub bottom_right: ImageSliceMut<'a>,
+
+}
+
 enum Orientation {
     I, H, V, HV, R, RH, RV, RHV
 }
+
+impl<'a> ImageSlice<'a> {
+
+    pub fn transform_pixel_index(&self) -> Matrix<u8>{
+        let (off_x, off_y)= self.spec.offset;
+        let (w, h) = self.spec.stride;
+        let dims = &self.spec.dims;
+
+        let mut buffer = Vec::new();
+
+        for j in 0..dims.height {
+            let offset = w * j * 4 + off_x + off_y*w*4;
+            for i in 0..dims.width * 4 {
+                buffer.push(self.pixels[(offset+i) as usize]);
+            }
+        }
+
+        buffer.into_boxed_slice()
+    }
+}
+
+/****************************************** Functions *************************************************/
+/******************************************************************************************************/
 
 pub fn import() {
     let img = match rust_image::open("test.png") {
@@ -248,15 +293,15 @@ pub fn import() {
         Err(err) => return dispatch_error(ImageError::InvalidFormat)
     };
 
-//    println!("original image is: {:?}", img.raw_pixels());
-    let image = OwnedImage::new(img);
-    println!("width is: {}", image.dims.width);
-    println!("height is: {}", image.dims.height);
+    let image = OwnedImage::import(img);
 
-    let slice = image.crop(50, 100, Dimensions {width: 300, height: 500 });
-    slice.generate_image();
+    let flipped = image.flipped(false, true);
+    flipped.save_image();
 }
 
+
+/**************************************** Error Handling **********************************************/
+/******************************************************************************************************/
 
 fn dispatch_error(err: ImageError) {
     println!("{}", err);
@@ -270,9 +315,9 @@ pub enum ImageError {
     #[snafu(display("Can't parse the raw pixel vector into a matrix"))]
     ParseError,
 
-    #[snafu(display("Can't parse the raw pixel vector into a matrix"))]
+    #[snafu(display("Index out of bound"))]
     IndexOutOfBound,
 
-    #[snafu(display("Can not perform cropping"))]
-    CropFailed,
+    #[snafu(display("Can not perform image operation"))]
+    ImageOperationFailed,
 }
